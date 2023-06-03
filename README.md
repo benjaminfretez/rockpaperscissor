07a47236838fe699071c883c5e968c9073e5114a
Benjamin <fretezbenjamin6@gmail.com>
2023-06-02 22:59:43 -0300

    Rock-Paper-Scissor v1

---
+++ README.md
@@ -0,0 +1,42 @@
+## introduction
+
+In this script you play against the computer the classic Rock-Paper-Scissors game. 
+
+![The script in action](attachments/Pasted%20image%2020230602204055.png)
+
+## how it works
+
+When you run the script it will ask you which of Rock💎, Paper🧻 or Scissors✂, which it will play against the random computer choice through the `Get-Random` PowerShell function.
+
+![Alt text](attachments/Pasted%20image%2020230602204354.png)
+
+In your terminal will appear representations of Rock💎, Paper🧻 or Scissors✂ labeled with the names **You've selected'** and **Computer**, followed by a red text telling you the final result.
+
+![Alt text](attachments/Pasted%20image%2020230602210656.png)
+
+
+## inputs
+
+* Rock: *"r", "rock"*
+* Paper: *"p", "paper"*
+* Scissors: *"s", "scissor", "scissors"
+
+## testing purposes stuff
+
+There are some useful parameters that will help you test this program:
+
+* `-fc` *(Force computer)* after this parameter you can force the computer play. You can use the inputs above to use this parameter.
+
+* `-lang` *(Language)* you can set another language using this parameter, in this script is only avaliable spanish ("es", "español", "espanol", "spanish"). There are inputs for this language such as *"piedra", "o", "papel", "p", "tijera", "t"*.
+
+* `-help` and `-about` are common parameters with no need to explain.
+
+This script has been done under PowerShell 7.3.4 (not Windows PowerShell but it does work there too)
+It's needed to be main.ps1 and variables.ps1 together on the same folder.
+
+## contact
+
+Any questions or concerns are welcome!
+
+---
+thepshecker
---
+++ main.ps1
@@ -0,0 +1,166 @@
+# RPS_PPT (Rock Paper Scissor, Piedra Papel Tijera)
+# Author: thepshecker (benjaminfretez)
+
+<#
+.SYNOPSIS
+    Rock Paper Scissor Game
+.DESCRIPTION
+    This script allows you to play a Rock Paper Scissor game against the computer.
+.PARAMETER lang
+    Specifies the language to use for the game. Available options are "english" and "spanish".
+.PARAMETER help
+    Displays the help information.
+.PARAMETER fc
+    Forces the computer to choose a specific move. Available options are, for instance, "rock", "paper", and "scissor".
+.EXAMPLE
+    .\main.ps1 -lang english # Play in English (Default)
+.EXAMPLE
+    .\main.ps1 -lang spanish # Play in Spanish
+.EXAMPLE
+    Play the game, forcing the computer to choose "rock".
+    .\main.ps1 -fc rock
+#>
+
+param(
+	[string]$lang, # Language Select
+	[switch]$help, # Help Switch
+	[string]$fc, # Force Computer
+	[switch]$about, # About this Program
+	[switch]$ansi
+)
+
+function warn([string]$message){
+	Write-Host -NoNewline "Warning: " -ForegroundColor Yellow
+	Write-Host $message -ForegroundColor White
+}
+
+function red([string]$message){
+	Write-Host $message -ForegroundColor Red
+}
+
+function tip([string]$message){
+	Write-Host -NoNewline "Tip: " -ForegroundColor Green
+	Write-Host $message -ForegroundColor White
+}
+
+. .\variables.ps1 
+# The dot sourcing feature lets you run a script in the
+# current scope instead of in the script scope. When you 
+# run a script that is dot sourced, the commands in the 
+# script run as though you had typed them at the command prompt.
+
+# Microsoft Docs
+
+if (-not $english){
+	red "There's not variables.ps1 or it's something incorrect"
+	exit
+}
+
+if ($help){
+	#Write-Host "No help! Go find it somewhere else."
+
+	Get-Help -Name $MyInvocation.MyCommand.Path -Full
+	exit
+}
+elseif($about){
+	if($ansi){ 
+		$PSStyle.OutputRendering = 'Ansi'
+		Write-Host -NoNewLine $astrucolor 
+	}
+	else{ Write-Host $astrukot }
+	Write-Host "Made with love <3, thepshecker (benjaminfretez)"
+	exit
+}
+
+if ($lang -in "es", "español", "espanol", "spanish"){
+	# Language is Spanish.
+	#warn "hi"
+	$langstr = $spanish
+}
+elseif ($lang -in "en", "english"){
+	tip "Did you know English is the Default language for this game? :)"
+	$langstr = $english
+}
+elseif(-not $lang){
+	$langstr = $english
+}
+else{
+	warn "Wow! You talk that language? Sorry, but I don't know it."
+	$langstr = $english
+}
+
+$option = Read-Host $langstr.options
+
+if ($option -in "o", "piedra", "r", "rock", $langstr.rock)
+{
+	$yours = $piedra
+	Write-Output $langstr.chose $yours
+}
+elseif ($option -in "p", "papel", "paper", $langstr.paper)
+{
+	$yours = $papel
+	Write-Output $langstr.chose $yours
+}
+elseif ($option -in "i","s", "t", "tijera", "scissors", $langstr.scissor -or $option -like "sc*")
+{
+	$yours = $tijera
+	Write-Output $langstr.chose $yours
+}
+else
+{
+	$yours = "None"
+}
+
+$ppt = $piedra, $papel, $tijera
+$computer = $ppt | Get-Random 
+
+if($fc -in $rockpa){ $computer = $piedra }
+elseif($fc -in $pappar){ $computer = $papel }
+elseif($fc -in $scissp){ $computer = $tijera }
+elseif(-not $fc){}
+else{warn "Do you want also the computer wash your dishes?"}
+
+Write-Output $langstr.computer $computer
+
+if($yours -eq "None") { red $langstr.kick }
+
+if($yours -eq $computer){
+	red $langstr.tie 
+	exit
+}
+
+if($computer -eq $tijera)
+{
+	if($yours -eq $piedra)
+	{
+		red $langstr.win
+	}
+	elseif($yours -eq $papel)
+	{
+		red $langstr.lose
+	}
+}
+elseif($computer -eq $papel)
+{
+	if($yours -eq $tijera)
+	{
+		red $langstr.win
+	}
+	elseif($yours -eq $piedra)
+	{
+		red $langstr.lose
+	}
+}
+elseif($computer -eq $piedra)
+{
+	if($yours -eq $papel)
+	{
+		red $langstr.win
+	}
+	if($yours -eq $tijera)
+	{
+		red $langstr.lose
+	}
+}
+
+exit
